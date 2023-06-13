fa3b6687f64fb6775b7dd2a69d4e30bd762b8b16
Anant Narayan Patel <66427020+anantdark@users.noreply.github.com>
2023-06-13 16:17:38 +0000

    Update main.py
    Changed set_wallpaper to use gsettings.

--- bin/main.py
+++ bin/main.py
@@ -1,13 +1,13 @@
-from gi.repository import Gio
+# from gi.repository import Gio
 import urllib.request
 from bs4 import BeautifulSoup
 import os
-import gi
+# import gi
 import datetime
 import subprocess
 import time
 import random
-gi.require_version('Gio', '2.0')
+# gi.require_version('Gio', '2.0')
 
 ###################### TRYING TO IMPLEMENT SCHEDULING########################
 # subprocess.run(["xhost", "+SI:localuser:{}".format(os.getlogin())])
@@ -15,12 +15,15 @@ gi.require_version('Gio', '2.0')
 
 #############################################################################
 
-
 def set_wallpaper(filepath):
-    gsettings = Gio.Settings.new('org.gnome.desktop.background')
-    gsettings.set_string('picture-uri', f'file://{filepath}')
-    gsettings.apply()
-
+    try:
+        command = ['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', f'file://{filepath}']
+        subprocess.run(command)
+    except Exception as e:
+        print(e)
+#         gsettings = Gio.Settings.new('org.gnome.desktop.background')
+#         gsettings.set_string('picture-uri', f'file://{filepath}')
+#         gsettings.apply()
 
 def change_wallpaper(day):
     while True:
